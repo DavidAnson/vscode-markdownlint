2a7e886261f23d37de5756bf1adc4b977c0652e0
David Anson <david@dlaa.me>
2015-11-21 00:30:02 -0800

    Add code action (light bulb) links and info for warnings.

--- extension.js
+++ extension.js
@@ -1,18 +1,28 @@
 var vscode = require("vscode");
 var markdownlint = require("markdownlint");
+var opn = require("opn");
 var packageJson = require("./package.json");
+var defaultConfig = require("./default-config.json");
 
+var extensionName = packageJson.name;
+var openLinkCommandName = extensionName + ".openLink";
+var markdownLanguageId = "markdown";
+var markdownlintRulesMd = "https://github.com/DavidAnson/markdownlint/blob/master/doc/Rules.md";
 var newLineRe = /\r\n|\r|\n/;
 var resultLineRe = /^document: (\d+): (MD\d\d\d) (.*)$/;
+var messageCodeSuffixRe = /:.*$/;
+var diagnosticCollection = null;
 
 function lint(document) {
+	if (document.languageId !== markdownLanguageId) {
+		return;
+	}
+
 	var options = {
 		"strings": {
 			"document": document.getText()
 		},
-		"config": {
-			"MD013": false
-		}
+		"config": defaultConfig
 	};
 	var diagnostics = [];
 
@@ -28,21 +38,40 @@ function lint(document) {
 				var description = match[3];
 				var message = rule + ": " + description;
 				var diagnostic = new vscode.Diagnostic(document.lineAt(lineNumber).range, message, 1 /* Warning */);
+				diagnostic.code = markdownlintRulesMd + "#" + rule.toLowerCase() + "---" + description.toLowerCase().replace(/ /g, "-");
 				diagnostics.push(diagnostic);
 			}
 		});
 
-	var diagnosticCollection = vscode.languages.createDiagnosticCollection(packageJson.name);
 	diagnosticCollection.set(document.uri, diagnostics);
 }
 
-function activate(context) {
+function provideCodeActions(document, range, codeActionContext, cancellationToken) {
+	var diagnostics = codeActionContext.diagnostics || [];
+	return diagnostics.map(function forDiagnostic(diagnostic) {
+		return {
+			title: "More information about " + diagnostic.message.replace(messageCodeSuffixRe, "") + "...",
+			command: openLinkCommandName,
+			arguments: [ diagnostic.code ]
+		}
+	});
+}
+
+function activate(context) {	
 	context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(function didOpenTextDocument(document) {
 		lint(document);
 	}));
 	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(function didChangeTextDocument(change) {
 		lint(change.document);
 	}));
+	context.subscriptions.push(vscode.languages.registerCodeActionsProvider(markdownLanguageId, {
+		provideCodeActions: provideCodeActions
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand(openLinkCommandName, function(link) {
+		opn(link);
+	}));
+	diagnosticCollection = vscode.languages.createDiagnosticCollection(extensionName);
+	context.subscriptions.push(diagnosticCollection);
 }
 
 exports.activate = activate;
