561ceb868222fbe4d1dbb1ddcd13c389b8c1f94b
David Anson <david@dlaa.me>
2023-09-28 21:29:53 -0700

    Add simple test for lintWorkspace command, run it only when workspace is available.

--- test/tests.cjs
+++ test/tests.cjs
@@ -44,106 +44,127 @@ function callbackWrapper (reject, callback) {
 	}
 }
 
-const tests = [
+function openLintEditVerifyFixAll () {
+	return testWrapper((resolve, reject, disposables) => {
+		let fixedAll = false;
+		disposables.push(
+			vscode.window.onDidChangeActiveTextEditor((textEditor) => {
+				callbackWrapper(reject, () => {
+					assert.ok(textEditor.document.uri.path.endsWith("/README.md"));
+					textEditor.edit((editBuilder) => {
+						// MD019
+						editBuilder.insert(new vscode.Position(0, 1), " ");
+						// MD012
+						editBuilder.insert(new vscode.Position(1, 0), "\n");
+					});
+				});
+			}),
+			vscode.languages.onDidChangeDiagnostics((diagnosticChangeEvent) => {
+				callbackWrapper(reject, () => {
+					const {uris} = diagnosticChangeEvent;
+					assert.equal(uris.length, 1);
+					const [ uri ] = uris;
+					const diagnostics = vscode.languages.getDiagnostics(uri);
+					if ((diagnostics.length > 0) && !fixedAll) {
+						// eslint-disable-next-line array-element-newline
+						const [ md019, md012 ] = diagnostics;
+						// @ts-ignore
+						assert.equal(md019.code.value, "MD019");
+						assert.equal(
+							// @ts-ignore
+							md019.code.target.toString(),
+							"https://github.com/DavidAnson/markdownlint/blob/v0.31.1/doc/md019.md"
+						);
+						assert.equal(
+							md019.message,
+							"MD019/no-multiple-space-atx: Multiple spaces after hash on atx style heading"
+						);
+						assert.ok(md019.range.isEqual(new vscode.Range(0, 0, 0, 4)));
+						assert.equal(md019.severity, vscode.DiagnosticSeverity.Warning);
+						assert.equal(md019.source, "markdownlint");
+						// @ts-ignore
+						assert.equal(md012.code.value, "MD012");
+						assert.equal(
+							// @ts-ignore
+							md012.code.target.toString(),
+							"https://github.com/DavidAnson/markdownlint/blob/v0.31.1/doc/md012.md"
+						);
+						assert.equal(
+							md012.message,
+							"MD012/no-multiple-blanks: Multiple consecutive blank lines [Expected: 1; Actual: 2]"
+						);
+						assert.ok(md012.range.isEqual(new vscode.Range(2, 0, 2, 0)));
+						assert.equal(md012.severity, vscode.DiagnosticSeverity.Warning);
+						assert.equal(md012.source, "markdownlint");
+						vscode.commands.executeCommand("markdownlint.fixAll");
+						fixedAll = true;
+					} else if ((diagnostics.length === 0) && fixedAll) {
+						resolve();
+					}
+				});
+			})
+		);
+		vscode.window.showTextDocument(vscode.Uri.file(path.join(__dirname, "..", "README.md")));
+	});
+}
 
-	function openLintEditVerifyFixAll () {
-		return testWrapper((resolve, reject, disposables) => {
-			let fixedAll = false;
-			disposables.push(
-				vscode.window.onDidChangeActiveTextEditor((textEditor) => {
-					callbackWrapper(reject, () => {
+function openLintEditCloseClean () {
+	return testWrapper((resolve, reject, disposables) => {
+		let closedActiveEditor = false;
+		disposables.push(
+			vscode.window.onDidChangeActiveTextEditor((textEditor) => {
+				callbackWrapper(reject, () => {
+					if (textEditor) {
 						assert.ok(textEditor.document.uri.path.endsWith("/README.md"));
 						textEditor.edit((editBuilder) => {
-							// MD019
 							editBuilder.insert(new vscode.Position(0, 1), " ");
-							// MD012
 							editBuilder.insert(new vscode.Position(1, 0), "\n");
 						});
-					});
-				}),
-				vscode.languages.onDidChangeDiagnostics((diagnosticChangeEvent) => {
-					callbackWrapper(reject, () => {
-						const {uris} = diagnosticChangeEvent;
-						assert.equal(uris.length, 1);
-						const [ uri ] = uris;
-						const diagnostics = vscode.languages.getDiagnostics(uri);
-						if ((diagnostics.length > 0) && !fixedAll) {
-							// eslint-disable-next-line array-element-newline
-							const [ md019, md012 ] = diagnostics;
-							// @ts-ignore
-							assert.equal(md019.code.value, "MD019");
-							assert.equal(
-								// @ts-ignore
-								md019.code.target.toString(),
-								"https://github.com/DavidAnson/markdownlint/blob/v0.31.1/doc/md019.md"
-							);
-							assert.equal(
-								md019.message,
-								"MD019/no-multiple-space-atx: Multiple spaces after hash on atx style heading"
-							);
-							assert.ok(md019.range.isEqual(new vscode.Range(0, 0, 0, 4)));
-							assert.equal(md019.severity, vscode.DiagnosticSeverity.Warning);
-							assert.equal(md019.source, "markdownlint");
-							// @ts-ignore
-							assert.equal(md012.code.value, "MD012");
-							assert.equal(
-								// @ts-ignore
-								md012.code.target.toString(),
-								"https://github.com/DavidAnson/markdownlint/blob/v0.31.1/doc/md012.md"
-							);
-							assert.equal(
-								md012.message,
-								"MD012/no-multiple-blanks: Multiple consecutive blank lines [Expected: 1; Actual: 2]"
-							);
-							assert.ok(md012.range.isEqual(new vscode.Range(2, 0, 2, 0)));
-							assert.equal(md012.severity, vscode.DiagnosticSeverity.Warning);
-							assert.equal(md012.source, "markdownlint");
-							vscode.commands.executeCommand("markdownlint.fixAll");
-							fixedAll = true;
-						} else if ((diagnostics.length === 0) && fixedAll) {
-							resolve();
-						}
-					});
-				})
-			);
-			vscode.window.showTextDocument(vscode.Uri.file(path.join(__dirname, "..", "README.md")));
-		});
-	},
+					}
+				});
+			}),
+			vscode.languages.onDidChangeDiagnostics((diagnosticChangeEvent) => {
+				callbackWrapper(reject, () => {
+					const uris = diagnosticChangeEvent.uris.filter((uri) => uri.scheme === "file");
+					assert.equal(uris.length, 1);
+					const [ uri ] = uris;
+					const diagnostics = vscode.languages.getDiagnostics(uri);
+					if ((diagnostics.length > 0) && !closedActiveEditor) {
+						vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+						closedActiveEditor = true;
+					} else if ((diagnostics.length === 0) && closedActiveEditor) {
+						resolve();
+					}
+				});
+			})
+		);
+		vscode.window.showTextDocument(vscode.Uri.file(path.join(__dirname, "..", "README.md")));
+	});
+}
 
-	function openLintEditCloseClean () {
-		return testWrapper((resolve, reject, disposables) => {
-			let closedActiveEditor = false;
-			disposables.push(
-				vscode.window.onDidChangeActiveTextEditor((textEditor) => {
-					callbackWrapper(reject, () => {
-						if (textEditor) {
-							assert.ok(textEditor.document.uri.path.endsWith("/README.md"));
-							textEditor.edit((editBuilder) => {
-								editBuilder.insert(new vscode.Position(0, 1), " ");
-								editBuilder.insert(new vscode.Position(1, 0), "\n");
-							});
-						}
-					});
-				}),
-				vscode.languages.onDidChangeDiagnostics((diagnosticChangeEvent) => {
-					callbackWrapper(reject, () => {
-						const uris = diagnosticChangeEvent.uris.filter((uri) => uri.scheme === "file");
-						assert.equal(uris.length, 1);
-						const [ uri ] = uris;
-						const diagnostics = vscode.languages.getDiagnostics(uri);
-						if ((diagnostics.length > 0) && !closedActiveEditor) {
-							vscode.commands.executeCommand("workbench.action.closeActiveEditor");
-							closedActiveEditor = true;
-						} else if ((diagnostics.length === 0) && closedActiveEditor) {
-							resolve();
-						}
-					});
-				})
-			);
-			vscode.window.showTextDocument(vscode.Uri.file(path.join(__dirname, "..", "README.md")));
-		});
-	}
+function lintWorkspace () {
+	return testWrapper((resolve, reject, disposables) => {
+		disposables.push(
+			vscode.window.onDidOpenTerminal((terminal) => {
+				callbackWrapper(reject, () => {
+					assert.equal(terminal.name, "Lint all Markdown files in the workspace with markdownlint");
+					// Unable to examine contents of terminal using VS Code API
+					resolve();
+				});
+			})
+		);
+		vscode.commands.executeCommand("markdownlint.lintWorkspace");
+	});
+}
 
+const tests = [
+	openLintEditVerifyFixAll,
+	openLintEditCloseClean
 ];
+if (vscode.workspace.workspaceFolders) {
+	tests.push(
+		lintWorkspace
+	);
+}
 
 module.exports = {tests};
