00919ad27a0aed4e345991d07ffb538931818b7d
David Anson <david@dlaa.me>
2024-02-08 22:27:29 -0800

    Add more unit tests for stringify-error module, refactor, output test run coverage (unenforced).

--- stringify-error.mjs
+++ stringify-error.mjs
@@ -1,12 +1,25 @@
 // @ts-check
 
 /**
+ * Regular expression for matching line endings.
+ */
+const newlineRe = /\r\n?|\n/g;
+
+/**
+ * Converts a string to an array of indented lines.
+ * @param {String} str String to convert.
+ * @returns {String[]} Array of indented lines.
+ */
+function toIndentedLines (str) {
+	return str.split(newlineRe).map((s) => ` ${s}`);
+}
+
+/**
  * String-ifies an Error (or AggregateError) object.
  * @param {Error} error Error object to string-ify.
- * @param {Number} [indent] Spaces to start indent.
  * @returns {String} Error details.
  */
-function stringifyError (error, indent = 0) {
+function stringifyError (error) {
 	const name = error?.name || "[NO NAME]";
 	const message = error?.message || JSON.stringify(error);
 	const stack = error?.stack || "[NO STACK]";
@@ -15,21 +28,22 @@ function stringifyError (error, indent = 0) {
 	// @ts-ignore
 	const errors = error?.errors || [];
 	const result = [ `${name}: ${message}`, "stack:" ];
-	const frames = stack.split(/\r\n?|\n/g);
-	for (const frame of frames.slice((frames[0] === result[0]) ? 1 : 0)) {
+	const frames = stack.split(newlineRe);
+	const discardFrame = (frames[0] === result[0]);
+	for (const frame of frames.slice(discardFrame ? 1 : 0)) {
 		result.push(` ${frame.trim()}`);
 	}
 	if (cause) {
 		result.push("cause:")
-		result.push(stringifyError(cause, indent + 1))
+		result.push(...toIndentedLines(stringifyError(cause)))
 	}
 	if (errors.length > 0) {
 		result.push("errors:")
 		for (const err of errors) {
-			result.push(stringifyError(err, indent + 1))
+			result.push(...toIndentedLines(stringifyError(err)))
 		}
 	}
-	return result.map((s) => `${" ".repeat(indent)}${s}`).join("\n");
+	return result.join("\n");
 }
 
 export default stringifyError;
--- test/stringify-error-test.mjs
+++ test/stringify-error-test.mjs
@@ -1,149 +1,190 @@
 // @ts-check
 
 import { default as assert } from "node:assert/strict";
-import { test } from "node:test";
+import { describe, test } from "node:test";
 import { default as stringifyError } from "../stringify-error.mjs";
 
-test("null", () => {
-	const error = null;
-	const actual = stringifyError(error);
-	const expected = "[NO NAME]: null\nstack:\n [NO STACK]";
-	assert.equal(actual, expected);
-});
+describe("stringify-error", () => {
 
-test("number", () => {
-	const error = 10;
-	// @ts-ignore
-	const actual = stringifyError(error);
-	const expected = `[NO NAME]: ${error}\nstack:\n [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("null", () => {
+		const error = null;
+		const actual = stringifyError(error);
+		const expected = "[NO NAME]: null\nstack:\n [NO STACK]";
+		assert.equal(actual, expected);
+	});
 
-test("string", () => {
-	const error = "STRING";
-	// @ts-ignore
-	const actual = stringifyError(error);
-	const expected = `[NO NAME]: "${error}"\nstack:\n [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("number", () => {
+		const error = 10;
+		// @ts-ignore
+		const actual = stringifyError(error);
+		const expected = `[NO NAME]: 10\nstack:\n [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("object", () => {
-	const error = {};
-	// @ts-ignore
-	const actual = stringifyError(error);
-	const expected = `[NO NAME]: ${JSON.stringify(error)}\nstack:\n [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("string", () => {
+		const error = "STRING";
+		// @ts-ignore
+		const actual = stringifyError(error);
+		const expected = `[NO NAME]: "STRING"\nstack:\n [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name", () => {
-	const error = {
-		"name": "NAME"
-	};
-	// @ts-ignore
-	const actual = stringifyError(error);
-	const expected = `${error.name}: ${JSON.stringify(error)}\nstack:\n [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("object", () => {
+		const error = {};
+		// @ts-ignore
+		const actual = stringifyError(error);
+		const expected = `[NO NAME]: {}\nstack:\n [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name/message", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE"
-	};
-	const actual = stringifyError(error);
-	const expected = `${error.name}: ${error.message}\nstack:\n [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("Error-like, name", () => {
+		const error = {
+			"name": "NAME"
+		};
+		// @ts-ignore
+		const actual = stringifyError(error);
+		const expected = `NAME: {"name":"NAME"}\nstack:\n [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name/message/stack", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE",
-		"stack": "STACK0\nSTACK1\nSTACK2"
-	};
-	const actual = stringifyError(error);
-	const expected = `NAME: MESSAGE\nstack:\n STACK0\n STACK1\n STACK2`;
-	assert.equal(actual, expected);
-});
+	test("Error-like, name/message", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE"
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name/message/stack-with-heading", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE",
-		"stack": "NAME: MESSAGE\nSTACK0\nSTACK1\nSTACK2"
-	};
-	const actual = stringifyError(error);
-	const expected = `NAME: MESSAGE\nstack:\n STACK0\n STACK1\n STACK2`;
-	assert.equal(actual, expected);
-});
+	test("Error-like, name/message/stack", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"stack": "STACK0\nSTACK1\nSTACK2"
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n STACK0\n STACK1\n STACK2`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name/message/cause", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE",
-		"cause": {
-			"name": "name",
-			"message": "message"
-		}
-	};
-	const actual = stringifyError(error);
-	const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\ncause:\n name: message\n stack:\n  [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("Error-like, name/message/stack-with-heading", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"stack": "NAME: MESSAGE\nSTACK0\nSTACK1\nSTACK2"
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n STACK0\n STACK1\n STACK2`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name/message/errors-none", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE",
-		"errors": []
-	};
-	const actual = stringifyError(error);
-	const expected = `NAME: MESSAGE\nstack:\n [NO STACK]`;
-	assert.equal(actual, expected);
-});
-
-test("Error-like, name/message/errors-one", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE",
-		"errors": [
-			{
+	test("Error-like, name/message/cause", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"cause": {
 				"name": "name",
 				"message": "message"
 			}
-		]
-	};
-	const actual = stringifyError(error);
-	const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\nerrors:\n name: message\n stack:\n  [NO STACK]`;
-	assert.equal(actual, expected);
-});
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\ncause:\n name: message\n stack:\n  [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("Error-like, name/message/errors-two", () => {
-	const error = {
-		"name": "NAME",
-		"message": "MESSAGE",
-		"errors": [
-			{
-				"name": "name0",
-				"message": "message0"
-			},
-			"string"
-		]
-	};
-	const actual = stringifyError(error);
-	const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\nerrors:\n name0: message0\n stack:\n  [NO STACK]\n [NO NAME]: "string"\n stack:\n  [NO STACK]`;
-	assert.equal(actual, expected);
-});
+	test("Error-like, name/message/errors-none", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"errors": []
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n [NO STACK]`;
+		assert.equal(actual, expected);
+	});
 
-test("Error, nested", () => {
-	// @ts-ignore
-	const error = new Error("MESSAGE0", { "cause": new Error("MESSAGE1") } );
-	const actual =
-		stringifyError(error).
+	test("Error-like, name/message/errors-one", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"errors": [
+				{
+					"name": "name",
+					"message": "message"
+				}
+			]
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\nerrors:\n name: message\n stack:\n  [NO STACK]`;
+		assert.equal(actual, expected);
+	});
+
+	test("Error-like, name/message/errors-two", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"errors": [
+				{
+					"name": "name",
+					"message": "message"
+				},
+				"string"
+			]
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\nerrors:\n name: message\n stack:\n  [NO STACK]\n [NO NAME]: "string"\n stack:\n  [NO STACK]`;
+		assert.equal(actual, expected);
+	});
+
+	test("Error-like, name/message/errors-nested", () => {
+		const error = {
+			"name": "NAME",
+			"message": "MESSAGE",
+			"errors": [
+				{
+					"name": "name",
+					"message": "message",
+					"cause": {
+						"name": "Name",
+						"message": "Message",
+					},
+					"errors": [
+						{
+							"name": "NaMe",
+							"message": "MeSsAgE",
+						}
+					]
+				}
+			]
+		};
+		const actual = stringifyError(error);
+		const expected = `NAME: MESSAGE\nstack:\n [NO STACK]\nerrors:\n name: message\n stack:\n  [NO STACK]\n cause:\n  Name: Message\n  stack:\n   [NO STACK]\n errors:\n  NaMe: MeSsAgE\n  stack:\n   [NO STACK]`;
+		assert.equal(actual, expected);
+	});
+
+	function generalize (output) {
+		return output.
 			split("\n").
 			filter((s) => !s.includes("at ") || s.includes("stringify-error-test")).
 			map((s) => s.replace(/^(\s*at ).*(stringify-error-test).*$/, "$1$2")).
 			join("\n");
-	const expected = "Error: MESSAGE0\nstack:\n at stringify-error-test\ncause:\n Error: MESSAGE1\n stack:\n  at stringify-error-test";
-	assert.equal(actual, expected);
+	}
+
+	test("Error, cause", () => {
+		// @ts-ignore
+		const error = new Error("MESSAGE0", { "cause": new Error("MESSAGE1") } );
+		const actual = generalize(stringifyError(error));
+		const expected = "Error: MESSAGE0\nstack:\n at stringify-error-test\ncause:\n Error: MESSAGE1\n stack:\n  at stringify-error-test";
+		assert.equal(actual, expected);
+	});
+
+	test("AggregateError, nested", () => {
+		// @ts-ignore
+		const error = new AggregateError([ new Error("MESSAGE1"), new AggregateError([ new Error("MESSAGE3") ], "MESSAGE2") ], "MESSAGE0");
+		const actual = generalize(stringifyError(error));
+		const expected = "AggregateError: MESSAGE0\nstack:\n at stringify-error-test\nerrors:\n Error: MESSAGE1\n stack:\n  at stringify-error-test\n AggregateError: MESSAGE2\n stack:\n  at stringify-error-test\n errors:\n  Error: MESSAGE3\n  stack:\n   at stringify-error-test";
+		assert.equal(actual, expected);
+	});
+
 });
