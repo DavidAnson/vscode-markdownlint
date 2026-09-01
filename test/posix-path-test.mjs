d0089d5e12c63be978a4a2607fc3f4df0592c29e
David Anson <david@dlaa.me>
2026-08-31 21:12:38 -0700

    Add unit tests for posixPath.

---
+++ test/posix-path-test.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+
+import path from "node:path";
+import posixPath from "../posix-path.mjs";
+import { describe, test } from "node:test";
+
+/* eslint-disable unicorn/prefer-string-raw */
+
+const testScenariosPosix = [
+	[ "/Users/user/Project", "/Users/user/Project" ],
+	[ "/Users/user/Project/file.txt", "/Users/user/Project/file.txt" ]
+];
+const testScenariosWin32 = [
+	[ "C:\\Users\\user\\Project", "C:/Users/user/Project" ],
+	[ "C:\\Users\\user\\Project\\file.txt", "C:/Users/user/Project/file.txt" ],
+	[ "\\\\hostname\\c$\\Project", "//hostname/c$/Project" ],
+	[ "\\\\hostname\\c$\\Project\\file.txt", "//hostname/c$/Project/file.txt" ]
+];
+
+describe("posix-path", () => {
+
+	test("POSIX scenarios", (t) => {
+		// eslint-disable-next-line node-test/no-conditional-in-test
+		if (path.sep === path.posix.sep) {
+			t.plan(testScenariosPosix.length);
+			for (const [ input, expected ] of testScenariosPosix) {
+				// eslint-disable-next-line node-test/no-conditional-assertion
+				t.assert.equal(posixPath(input), expected);
+			}
+		}
+	});
+
+	test("Win32 scenarios", (t) => {
+		// eslint-disable-next-line node-test/no-conditional-in-test
+		if (path.sep === path.win32.sep) {
+			t.plan(testScenariosWin32.length);
+			for (const [ input, expected ] of testScenariosWin32) {
+				// eslint-disable-next-line node-test/no-conditional-assertion
+				t.assert.equal(posixPath(input), expected);
+			}
+		}
+	});
+
+});
