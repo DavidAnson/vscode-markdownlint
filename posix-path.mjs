4787c47f698b5031dac3d0f0888962be6a074441
David Anson <david@dlaa.me>
2026-08-31 21:19:44 -0700

    Add sep parameter to posixPath to enable cross-platform testing.

--- posix-path.mjs
+++ posix-path.mjs
@@ -3,6 +3,6 @@
 import path from "node:path";
 
 // Converts to a POSIX-style path
-export default function posixPath (/** @type {string} */ anyPath) {
-	return anyPath.split(path.sep).join(path.posix.sep);
+export default function posixPath (/** @type {string} */ anyPath, /** @type {string} */ sep = path.sep) {
+	return anyPath.split(sep).join(path.posix.sep);
 }
--- test/posix-path-test.mjs
+++ test/posix-path-test.mjs
@@ -20,24 +20,18 @@ const testScenariosWin32 = [
 describe("posix-path", () => {
 
 	test("POSIX scenarios", (t) => {
-		// eslint-disable-next-line node-test/no-conditional-in-test
-		if (path.sep === path.posix.sep) {
-			t.plan(testScenariosPosix.length);
-			for (const [ input, expected ] of testScenariosPosix) {
-				// eslint-disable-next-line node-test/no-conditional-assertion
-				t.assert.equal(posixPath(input), expected);
-			}
+		t.plan(testScenariosPosix.length);
+		for (const [ input, expected ] of testScenariosPosix) {
+			// eslint-disable-next-line node-test/no-conditional-assertion
+			t.assert.equal(posixPath(input, path.posix.sep), expected);
 		}
 	});
 
 	test("Win32 scenarios", (t) => {
-		// eslint-disable-next-line node-test/no-conditional-in-test
-		if (path.sep === path.win32.sep) {
-			t.plan(testScenariosWin32.length);
-			for (const [ input, expected ] of testScenariosWin32) {
-				// eslint-disable-next-line node-test/no-conditional-assertion
-				t.assert.equal(posixPath(input), expected);
-			}
+		t.plan(testScenariosWin32.length);
+		for (const [ input, expected ] of testScenariosWin32) {
+			// eslint-disable-next-line node-test/no-conditional-assertion
+			t.assert.equal(posixPath(input, path.win32.sep), expected);
 		}
 	});
 
